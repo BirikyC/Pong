20cc5687f6e7bd38420ecc9914031e484a9fd04d
BirikyC <adam.ceglarz18@gmail.com>
2022-06-21 21:48:23 +0200

    ball

--- js/script.js
+++ js/script.js
@@ -13,8 +13,6 @@ $(window).resize(function(){
 	player[0].draw()
 })
 
-
-
 class Player{
 	constructor(pwidth, pheight, posx){
 		this.height = pheight
@@ -27,7 +25,7 @@ class Player{
 
 		this.position = {
 			x: posx,
-			y: canvas.height / 2 + this.height / 2
+			y: canvas.height / 2 - this.height / 2
 		}
 	}
 
@@ -44,6 +42,38 @@ class Player{
 	}
 }
 
+class Ball{
+	constructor(){
+		this.diameter = 2 * rem
+		this.color = "white"
+		this.velocity = {
+			x: 1.5 * rem,
+			y: 0
+		}
+
+		this.position = {
+			x: canvas.width / 2,
+			y: canvas.height / 2
+		}
+
+		let rand = Math.floor(Math.random() * 2)
+		if(rand > 0) this.velocity.x = -this.velocity.x
+	}
+
+	draw(){
+		ctx.beginPath()
+		ctx.arc(this.position.x, this.position.y, this.diameter, 0, 2 * Math.PI)
+		ctx.fillStyle = this.color
+		ctx.fill()
+	}
+
+	update(){
+		this.draw()
+		this.position.x += this.velocity.x
+		this.position.y += this.velocity.y
+	}
+}
+
 let player_width = 5 * rem
 let player_height = 25 * rem
 let player_posx = 8 * rem + player_width
@@ -51,12 +81,28 @@ let player = [null]
 player[0] = new Player(player_width, player_height, player_posx)
 player[1] = new Player(player_width, player_height, canvas.width - player_width - player_posx)
 
+const ball = new Ball()
+
 function animation(){
+	if(ball.position.x + ball.diameter < -ball.diameter || ball.position.x - ball.diameter > canvas.width + ball.diameter) return
 	requestAnimationFrame(animation)
+
+	if(ball.position.x - ball.diameter / 2 <= player[0].position.x + player_width && ball.position.x >= player[0].position.x && ball.position.y - ball.diameter <= player[0].position.y + player_height && ball.position.y >= player[0].position.y && ball.velocity.x < 0){
+		hitBall(0)
+	}
+	else if(ball.position.x + ball.diameter / 2 >= player[1].position.x && ball.position.x <= player[1].position.x + player_width && ball.position.y - ball.diameter <= player[1].position.y + player_height && ball.position.y >= player[1].position.y && ball.velocity.x > 0){
+		hitBall(1)
+	}
+
+	if(ball.position.y >= canvas.height && ball.velocity.y > 0 || ball.position.y <= 0 && ball.velocity.y < 0){
+		ball.velocity.y = -ball.velocity.y
+	}
+
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	for(let i=0; i<=1; i++){
 		playerMovement(i)
 	}
+	ball.update()
 }
 
 function playerMovement(num){
@@ -65,6 +111,13 @@ function playerMovement(num){
 	}
 
 	player[num].update()
+}
+
+function hitBall(num){
+	ball.velocity.x = -ball.velocity.x
+	ball.velocity.x *= 1.07
+
+	ball.velocity.y = (player[num].position.y + player_height / 2 - ball.position.y) / -6
 }
 
 animation()
