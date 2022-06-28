07426b46df9ac7815535e1b16c4497af025a28fb
BirikyC <adam.ceglarz18@gmail.com>
2022-06-28 21:17:46 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -92,7 +92,7 @@ setTimeout(() => {
 function gameLoop(){
 	if(ball.position.x + ball.diameter < -ball.diameter || ball.position.x - ball.diameter > canvas.width + ball.diameter){
 		let score = 0
-		let path = $('.score-points > span:first-child')
+		let path = $('.score-points > span')[0]
 		if(ball.position.x < canvas.width / 2){
 			score = parseInt($('.score-points > span:first-child').text())
 			score++
