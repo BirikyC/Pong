a57ac7d2b42b44ee59121447d1402a08413a73a1
BirikyC <adam.ceglarz18@gmail.com>
2022-06-28 04:43:39 +0200

    code ordering

---
+++ src/settings.js
@@ -0,0 +1,46 @@
+$(function(){
+    $('input[type="number"]').change(function(){
+        if(this.value > 99) this.value = 99
+    })
+})
+
+$(function(){
+    let colors = ['White', 'Green', 'Blue', 'Red']
+    let rgb = ['rgb(255, 255, 255)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)', 'rgb(255, 0, 0)']
+    let actColor = 0
+
+    $('.color').click(function(){
+        actColor++
+        if(actColor >= colors.length) actColor = 0
+        
+        $(this).html(colors[actColor])
+        $('.text').css({color: rgb[actColor]})
+        console.log(actColor)
+    })
+
+    const bgColors = ['Black', 'White']
+    const themes = ['Dark', 'Light']
+    let actTheme = 0
+
+    $('.theme').click(function(){
+        actTheme++
+        if(actTheme >= themes.length) actTheme = 0
+
+        $(this).html(themes[actTheme])
+        
+        if(actTheme === 0){
+            colors[0] = 'White'
+            rgb[0] = 'rgb(255, 255, 255)'
+        }
+        else{
+            colors[0] = 'Black'
+            rgb[0] = 'rgb(0, 0, 0)'
+        }
+
+        $('body').css({backgroundColor: bgColors[actTheme]})
+        if(actColor != 0) return
+
+        $('.color').html(colors[actColor])
+        $('.text').css({color: rgb[actColor]})
+    })
+})
